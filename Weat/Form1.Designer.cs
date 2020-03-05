32515c13641e48630601dd0e19b0689dacd44833
Omar Mohamud <qx2316io@go.minneapolis.edu>
2020-03-05 13:21:17 -0600

    Add project files.

---
+++ Weat/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Weat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGetWeather = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblWeather = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(276, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "State";
+            // 
+            // btnGetWeather
+            // 
+            this.btnGetWeather.Location = new System.Drawing.Point(590, 28);
+            this.btnGetWeather.Name = "btnGetWeather";
+            this.btnGetWeather.Size = new System.Drawing.Size(75, 23);
+            this.btnGetWeather.TabIndex = 2;
+            this.btnGetWeather.Text = "Get weather";
+            this.btnGetWeather.UseVisualStyleBackColor = true;
+            this.btnGetWeather.Click += new System.EventHandler(this.btnGetWeather_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(683, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lblWeather
+            // 
+            this.lblWeather.AutoSize = true;
+            this.lblWeather.Location = new System.Drawing.Point(250, 165);
+            this.lblWeather.Name = "lblWeather";
+            this.lblWeather.Size = new System.Drawing.Size(169, 13);
+            this.lblWeather.TabIndex = 4;
+            this.lblWeather.Text = "Enter city and state to get weather";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(64, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(646, 216);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(126, 30);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 6;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(360, 28);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(100, 20);
+            this.txtState.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.lblWeather);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnGetWeather);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Current weather";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGetWeather;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblWeather;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+    }
+}
+
---
+++ Weat/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weat
+{
+    public partial class Form1 : Form
+    {
+        readonly string BaseUrl = "https://weather-csharp.herokuapp.com/";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool GetWeatherText(string city, string state, out string weatherText, out string errorMessage)
+        {
+        http://weather-csharp.herokuapp.com/text?city=houston&state=tx
+
+            string weatherTextUrl = string.Format("{0}text?city={1}&state={2}", BaseUrl, city, state);
+          
+            Debug.WriteLine(weatherTextUrl);
+
+            errorMessage = null;
+            weatherText =null;
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    weatherText = client.DownloadString(weatherTextUrl);
+                }
+                Debug.WriteLine(weatherText);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                errorMessage = e.Message;
+                return false;
+            }
+           
+        }
+
+        private bool LocationDataValid (string city, string state)
+        {
+
+            
+            if (string.IsNullOrWhiteSpace(city))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(state))
+            {
+                return false;
+            }
+            return true;
+        }
+       
+        private bool GetWeatherImage (string city, string state, out Image weatherImage, out string errorMessage)
+        {
+            weatherImage = null;
+            errorMessage = null;
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                http://weather-csharp.herokuapp.com/photo?city=houston&state=tx
+                    string weatherPhotoUrl = string.Format("{0}photo?city={1}&state{2}", BaseUrl, city, state);
+                    string tempFileDirectory = Path.GetTempPath().ToString();
+                    string weatherFilePath = Path.Combine(tempFileDirectory, "weather_image.jpeg");
+                    Debug.WriteLine(weatherFilePath);
+                    client.DownloadFile(weatherPhotoUrl, weatherFilePath);
+                    weatherImage = Image.FromFile(weatherFilePath);
+
+                }
+                return true;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                errorMessage = e.Message;
+                return false;
+            }
+        }
+        private void btnGetWeather_Click(object sender, EventArgs e)
+        {
+            btnGetWeather.Enabled = false;
+
+            string city = txtCity.Text;
+            string state = txtState.Text;
+            
+            if (LocationDataValid(city, state))
+            {
+                if (GetWeatherText(city, state, out string weather, out string textErrorMessage))
+                {
+                    lblWeather.Text = weather;
+                }
+                else
+                {
+                    MessageBox.Show(textErrorMessage, "Error");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Enter both city and state", "Error");
+            }
+
+            if(pictureBox1.Image !=null)
+            {
+                pictureBox1.Image.Dispose();
+            }
+            if (GetWeatherImage(city, state, out Image image, out string imageErrorMessage))
+            {
+                pictureBox1.Image = image;
+            }
+            btnGetWeather.Enabled = true;
+        }
+    }
+}
